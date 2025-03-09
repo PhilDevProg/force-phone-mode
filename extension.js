921939ef6846e50ea3aa6f93d9491335b87e7208
PhilProg <91820316+PhilDevProg@users.noreply.github.com>
2025-03-09 23:02:52 +0100

    Add quick toggle

--- extension.js
+++ extension.js
@@ -16,18 +16,65 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
-import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import {
+  QuickToggle,
+  SystemIndicator,
+} from "resource:///org/gnome/shell/ui/quickSettings.js";
+import GObject from 'gi://GObject';
+
+
+function enablePhoneMode() {
+  Main.layoutManager._checkIsPhone = () => true;
+  Main.layoutManager._updateIsPhone();
+}
+
+function disablePhoneMode() {
+  Main.layoutManager._checkIsPhone = () => false;
+  Main.layoutManager._updateIsPhone();
+}
+
+const MobileToggle = GObject.registerClass(
+class MobileToggle extends QuickToggle {
+    _init(extensionObject) {
+        super._init({
+            title: _('Mobile mode'),
+            iconName: 'phone-symbolic',
+            toggleMode: true,
+        });
+
+        this.connect('clicked', () => {
+          if (this.checked) {
+            enablePhoneMode();
+          } else {
+            disablePhoneMode();
+          }
+        });
+    }
+});
+
+const Indicator = GObject.registerClass(
+  class Indicator extends SystemIndicator {
+    constructor() {
+      super();
+      const toggle = new MobileToggle();
+      this.quickSettingsItems.push(toggle);
+    }
+  }
+);
 
 export default class ForcePhoneModeExtension extends Extension {
   enable() {
-    this._oldCheckIsPhone = Main.layoutManager._checkIsPhone;
-    Main.layoutManager._checkIsPhone = () => true;
-    Main.layoutManager._updateIsPhone();
+    this._indicator = new Indicator();
+    Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
   }
 
   disable() {
-    Main.layoutManager._checkIsPhone = this._oldCheckIsPhone;
-    this._oldCheckIsPhone = undefined;
-    Main.layoutManager._updateIsPhone();
+    disablePhoneMode();
+    this._indicator.quickSettingsItems.forEach((item) => item.destroy());
+    this._indicator.destroy();
+    this._indicator = null;
   }
 }
